260d161f3d74e9f205ea3523fc7353c5b7dbf024
lucydevonne <wambuilucie7@gmail.com>
2024-08-11 06:49:20 -0400

    Integrate Clerk authentication and enhance frontend UI/UX

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
